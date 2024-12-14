9e57682aa37b06794d29004ff66f8b8f327857a7
George Roubie <george.roubie@gmail.com>
2024-12-14 17:19:07 +0200

    Add simple validation

--- src/components/pokemon-finder/search-pokemon-form/SearchPokemonForm.tsx
+++ src/components/pokemon-finder/search-pokemon-form/SearchPokemonForm.tsx
@@ -1,20 +1,56 @@
+import { useState } from 'react';
 import Button from '../../shared/button/Button';
 import styles from './SearchPokemonForm.module.css';
 
+const INPUT_ID = 'pokemon_name_input';
 const CHECKBOX_ID = 'accept_terms_checkbox';
+const MIN_CHARACTERS = 3;
 
 const SearchPokemonForm = () => {
+  const [pokemonName, setPokemonName] = useState('');
+  const [acceptTerms, setAcceptTerms] = useState(false);
+
+  const isButtonDisabled = !acceptTerms;
+
+  const handleOnSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+  };
+
+  // const handleOnAccept = () => {
+  //   setAcceptTerms(!acceptTerms);
+  // };
+
+  // const handleOnDecline = () => {
+  //   setAcceptTerms(false);
+  // }
+
   return (
-    <form className={styles.form}>
-      <input placeholder="Search for a Pokemon..." className={styles.input} />
+    <form className={styles.form} onSubmit={handleOnSubmit}>
+      <input
+        id={INPUT_ID}
+        value={pokemonName}
+        type="text"
+        autoComplete="off"
+        placeholder="Search for a Pokemon..."
+        required
+        minLength={MIN_CHARACTERS}
+        className={styles.input}
+        onChange={(e) => setPokemonName(e.target.value)}
+      />
+      <p className={styles.errorMessage}>Pokemon name must be at least {MIN_CHARACTERS} characters long.</p>
       <div className={styles.checkboxWrapper}>
-        <input type="checkbox" id={CHECKBOX_ID} name={CHECKBOX_ID} />
+        <input
+          type="checkbox"
+          id={CHECKBOX_ID}
+          name={CHECKBOX_ID}
+          onChange={({ target }) => setAcceptTerms(target.checked)}
+        />
         <label htmlFor={CHECKBOX_ID}>
           Accept&nbsp;
           <span>terms</span>
         </label>
       </div>
-      <Button>Search</Button>
+      <Button disabled={isButtonDisabled}>Search</Button>
     </form>
   );
 };
