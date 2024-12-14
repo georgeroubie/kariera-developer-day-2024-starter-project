cbaf66f4fd54cbcb5607e77da4bfa948b8662cf1
George Roubie <george.roubie@gmail.com>
2024-12-14 16:16:43 +0200

    Add global css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import './styles/base.css';
 import './styles/reset.css';
+import './styles/typography.css';
+import './styles/variables.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
